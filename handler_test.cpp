3de8e94ebd556e4b3596eb5346ced548345339e3
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-03-07 22:30:40 +0300

    UPD for refactor

--- handler_test.cpp
+++ handler_test.cpp
@@ -1,99 +1,7 @@
 #include <iostream>
-#include <iomanip>
+#include "ChessHandler.h"
 
 using namespace std;
-
-class ChessHandler {
-    private:
-
-    int position[8][8] = {
-        {5, 4, 3, 2, 1, 3, 4, 5},
-        {6, 6, 6, 6, 6, 6, 6, 6},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {-6, -6, -6, -6, -6, -6, -6, -6},
-        {-5, -4, -3, -2, -1, -3, -4, -5}
-    };
-
-    public:
-
-    void print() {
-        for (int i = 7; i >= 0; i--) {
-            cout << i << " | ";
-            for (int j = 0; j < 8; j++)
-            {
-                char out;
-                switch (abs(position[i][j]))
-                {
-                case 0:
-                    out = '.';
-                    break;
-
-                case 1:
-                    out = 'k';
-                    break;
-
-                case 2:
-                    out = 'q';
-                    break;
-
-                case 3:
-                    out = 'b';
-                    break;
-
-                case 4:
-                    out = 'n';
-                    break;
-
-                case 5:
-                    out = 'r';
-                    break;
-
-                case 6:
-                    out = 'p';
-                    break;
-                
-                default:
-                    out = '?';
-                    break;
-                }
-
-                out = position[i][j] > 0 ? toupper(out): out;
-                cout << out << " ";
-            }
-            cout << endl;
-        }
-
-        cout << "   ";
-        for (int i = 0; i < 8; i++) {
-            cout << " _";
-        }
-
-        cout << endl;
-
-        cout << "   ";
-        for (int i = 0; i < 8; i++) {
-            cout << " " << char('A' + i);
-        }
-
-        cout << endl;
-    }
-
-    void move(char m[4]) {
-        int i1 = m[0] - 'a';
-        int j1 = m[1] - '1';
-        int i2 = m[2] - 'a';
-        int j2 = m[3] - '1';
-
-        //TODO: check move
-        position[j2][i2] = position[j1][i1];
-        position[j1][i1] = 0;
-
-        print();
-    }
-};
 
 int main() {
     ChessHandler m;
@@ -102,7 +10,9 @@ int main() {
     while (true) {
         char s[4];
         cin >> s;
-        m.move(s);
+        if (m.move(s))
+            m.print();
+        else cout << "ERROR" << endl;
     }
 
     return 0;
--- main.cpp
+++ main.cpp
@@ -1,67 +1,29 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <cstdlib>
+#include "ChessHandler.h"
 
 using namespace sf;
 
-class ChessHandler {
-    private:
-
-    bool WhiteToMove = true;
-
-    int posititon[8][8] = {
-        5, 4, 3, 2, 1, 3, 4, 5,
-        6, 6, 6, 6, 6, 6, 6, 6,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        -6, -6, -6, -6, -6, -6, -6, -6, 
-        -5, -4, -3, -2, -1, -3, -4, -5
-    };
-
-    // const std::string errors[3] = {
-    //     "Success",
-    //     "Check",
-    //     ""
-    // };
-    public:
-
-    int getCell(int i, int j) { return posititon[i][j]; }
-
-    // std::string getError(int error_code) {return errors[error_code];}
-
-    // bool move(char* str_move) {
-    //     try {
-    //         str_move[0]
-    //     }
-    //     catch 
-    // }
-
-    bool move(std::pair<int, int> start, std::pair<int, int> end) {
-        //if (CheckToWhite && WhiteToMove && unsafe_move) { return false; }
-        return false;
-    }
-};
-
-int border_size = 504;
-int size = 56;
-//int border = 28;
-int border = (border_size - size * 8) / 2;
+int board_size = 504;
+int field_size = 56;
+int border = (board_size - field_size * 8) / 2;
 ChessHandler game;
 Sprite board[32];
 
 void loadPosition() {
     int k = 0;
-    for (int i = 0; i < 8; i++)
+    //for (int i = 0; i < 8; i++)
+    for (int i = 7; i >= 0; i--)
         for (int j = 0; j < 8; j++)
+        //for (int j = 7; j >= 0; j--)
         {
-            int piece_id = game.getCell(i, j);
+            int piece_id = game.get_cell(i, j);
             if (piece_id != 0) {
                 int x = abs(piece_id) - 1;
-                int y = piece_id > 0 ? 1 : 0;
-                board[k].setTextureRect(IntRect(size*x, size*y, size, size));
-                board[k].setPosition(size*j + border, size*i + border);
+                int y = piece_id > 0 ? 1 : 0; //TODO: check
+                board[k].setTextureRect(IntRect(field_size*x, field_size*y, field_size, field_size));
+                board[k].setPosition(field_size*j + border, field_size*i + border);
                 k++;
             }
         }
@@ -69,7 +31,7 @@ void loadPosition() {
 
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(border_size, border_size), "ChessSFML", sf::Style::Titlebar | sf::Style::Close);;
+    sf::RenderWindow window(sf::VideoMode(board_size, board_size), "ChessSFML", sf::Style::Titlebar | sf::Style::Close);;
     auto desktop = sf::VideoMode::getDesktopMode();
     window.setPosition(Vector2i(desktop.width/2 - window.getSize().x/2, desktop.height/2 - window.getSize().y/2));
 
@@ -110,8 +72,8 @@ int main() {
                             dx = pos.x - board[i].getPosition().x;
                             dy = pos.y - board[i].getPosition().y;
 
-                            move[0] = static_cast<int>('a') + (int) (board[moved_piece].getPosition().x / size);
-                            move[1] = std::to_string(8 - (int) (board[moved_piece].getPosition().y / size))[0];
+                            move[0] = static_cast<int>('a') + (int) (board[moved_piece].getPosition().x / field_size);
+                            move[1] = std::to_string(8 - (int) (board[moved_piece].getPosition().y / field_size))[0];
                         }
                     }
 
@@ -120,22 +82,31 @@ int main() {
                 {
                     isMove = false;
 
-                    Vector2f p = board[moved_piece].getPosition() + Vector2f(size/2 - border, size/2 - border);
-                    Vector2f newPosition = Vector2f(size*int(p.x/size) + border, size*int(p.y/size) + border);
-                    board[moved_piece].setPosition(newPosition);
+                    Vector2f p = board[moved_piece].getPosition() + Vector2f(field_size/2 - border, field_size/2 - border);
+                    Vector2f newPosition = Vector2f(field_size*int(p.x/field_size) + border, field_size*int(p.y/field_size) + border);
 
-                    move[2] = static_cast<int>('a') + (int) (newPosition.x / size);
-                    move[3] = std::to_string(8 - (int) (newPosition.y / size))[0];
+                    move[2] = static_cast<int>('a') + (int) (newPosition.x / field_size);
+                    move[3] = std::to_string(8 - (int) (newPosition.y / field_size))[0];
 
-                    std::cout << "White are trying move " << move << std::endl << std::endl;
-                    
-                    //TODO: update board data
+                    std::cout << "Trying move " << move << std::endl;
+                    std::cout << "Moved piece " << moved_piece << std::endl << std::endl;
+
+                    // if (game.move(move))
+                    //     board[moved_piece].setPosition(newPosition);
+                    // else loadPosition();
+
+                    if (game.move(move))
+                        board[moved_piece].setPosition(newPosition);
+                    //TODO: else board[moved_piece].setPosition(oldPosition);
+
+                    loadPosition();
+
+                    game.print();
                 }
 
             if (isMove) {
+                //todo
                 board[moved_piece].setPosition(pos.x - dx, pos.y - dy);
-                
-                
             }
             
         }
