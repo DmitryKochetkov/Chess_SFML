e8f19434569f2c776d8fac884f1f01d207d9a133
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-06-20 17:33:19 +0300

    Поддержка иконок FontAwesome

--- Label.h
+++ Label.h
@@ -17,27 +17,34 @@ class Label : public sf::Drawable {
 
 public:
     //TODO: Vector2f -> Vector2i
-    Label(const sf::Vector2f position, const std::wstring& text, const sf::Vector2f size = sf::Vector2f(200, 100)) {
+    Label(const sf::Vector2f position, const std::wstring& text, const sf::Vector2f size = sf::Vector2f(200, 50)) {
         body = sf::RectangleShape(size);
         body.setPosition(position);
         body.setFillColor(sf::Color(255, 255, 255, 255));
         body.setOutlineColor(sf::Color(0, 0, 0));
-        body.setOutlineThickness(3.0f);
+        body.setOutlineThickness(1.5f);
 
         this->text.setString(sf::String(text));
         this->text.setFont(ResourceHolder::Instance().getFont("PT Sans"));
+        this->text.setCharacterSize(22);
+        this->text.setStyle(sf::Text::Bold);
         this->text.setFillColor(sf::Color(0, 0, 0));
-        //int x;
-        sf::Vector2f offset; 
+
+        sf::Vector2f offset;
         offset.x = (float)this->text.getGlobalBounds().width;
         offset.y = (float)this->text.getGlobalBounds().height/0.5f;
         this->text.setPosition(position+size*0.5f - offset*0.5f);
-        std::cout << this->text.getGlobalBounds().width << std::endl;
+        std::cout << this->text.getGlobalBounds().width << std::endl; //TODO: зачем это вообще здесь?
     }
 
     template <class T>
     bool contains(sf::Vector2<T> point) {
         return body.getGlobalBounds().contains(point.x, point.y);
+    }
+
+    //temporary methods
+    void setFont(sf::Font& font) {
+        text.setFont(font);
     }
 
 protected:
--- main.cpp
+++ main.cpp
@@ -17,7 +17,9 @@ int border = (board_size - field_size * 8) / 2;
 int main() {
     //TODO for Kirill: где-то здесь ты должен начать кодить кнопки
 
-    sf::RenderWindow window(sf::VideoMode(board_size, board_size), "ChessSFML", sf::Style::Titlebar | sf::Style::Close);;
+    sf::ContextSettings appSettings;
+    appSettings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(board_size, board_size), "ChessSFML", sf::Style::Titlebar | sf::Style::Close, appSettings);
     auto desktop = sf::VideoMode::getDesktopMode();
     window.setPosition(Vector2i(desktop.width/2 - window.getSize().x/2, desktop.height/2 - window.getSize().y/2));
 
@@ -26,30 +28,39 @@ int main() {
     ResourceHolder::Instance().loadTexture("../board.png", "board");
     ResourceHolder::Instance().loadTexture("../pieces_resized.png", "pieces");
     ResourceHolder::Instance().loadFont("../resources/fonts/PT Sans.ttf", "PT Sans");
+    ResourceHolder::Instance().loadFont("../resources/fonts/font_awesome/fa-solid-900.ttf", "Font Awesome Solid");
 
     //Спрайты
 
-    Group menu;
-    Group game_group;
+    Group* menu = new Group();
+    Group* game_group = new Group();
+    Group* auth = new Group();
 
     sf::RectangleShape background(sf::Vector2f(board_size, board_size));
     background.setFillColor(sf::Color(255, 255, 255, 255));
 
-    Label start((sf::Vector2f(window.getSize().y * 0.5f - 100, 50)), L"Начать игру");
-    Label settings((sf::Vector2f(window.getSize().y * 0.5f - 100, 200)), L"Настройки");
+    Label login((sf::Vector2f(window.getSize().y * 0.5f - 100, 50)), L"Начать игру");
+    Label signup((sf::Vector2f(window.getSize().y * 0.5f - 100, 50)), L"Начать игру");
+    auth->push_back(login);
+    auth->push_back(signup);
+
+    Label start((sf::Vector2f(window.getSize().y * 0.5f - 100, 150)), L"Начать игру");
     Label exit((sf::Vector2f(window.getSize().y * 0.5f - 100, 350)), L"Выход");
-    menu.push_back(start);
-    menu.push_back(settings);
-    menu.push_back(exit);
+    Label settings_icon((sf::Vector2f(window.getSize().y - 100, 20)), L"\uf013", sf::Vector2f(50, 50));
+    settings_icon.setFont(ResourceHolder::Instance().getFont("Font Awesome Solid"));
+
+    menu->push_back(start);
+    menu->push_back(exit);
+    menu->push_back(settings_icon);
 
     Label label4((sf::Vector2f(window.getSize().y * 0.5f - 100, 50)), L"Против компьютера");
     Label label5((sf::Vector2f(window.getSize().y * 0.5f - 100, 200)), L"Против пользователя");
     Label label6((sf::Vector2f(window.getSize().y * 0.5f - 100, 350)), L"Лобби");
-    game_group.push_back(label4);
-    game_group.push_back(label5);
-    game_group.push_back(label6);
+    game_group->push_back(label4);
+    game_group->push_back(label5);
+    game_group->push_back(label6);
 
-    Group visible; //отрисовываемая группа
+    Group* visible; //отрисовываемая группа
     visible = menu;
 
     while (window.isOpen())
@@ -60,13 +71,22 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && start.contains(sf::Mouse::getPosition(window)))
-                visible = game_group;
+            if (visible == menu) {
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && exit.contains(sf::Mouse::getPosition(window)))
+                    window.close();
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && start.contains(sf::Mouse::getPosition(window)))
+                    visible = game_group;
+            }
+
+            if (visible == game_group) {
+
+            }
+
         }
 
         window.clear();
         window.draw(background);
-        window.draw(visible);
+        window.draw(*visible);
         window.display();
     }
 
