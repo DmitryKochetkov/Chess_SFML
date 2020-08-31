a6094dc698fe2ef0c313fe0275c005f3aa4f74ca
KirillSmirnov236 <58817071+KirillSmirnov236@users.noreply.github.com>
2020-08-31 12:35:52 +0300

    base MainMenu

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "MainMenu.h"
+
+MainMenu::MainMenu() {
+	body.setFillColor(FillColor);
+	body.setOutlineColor(BorderColor);
+	body.setOutlineThickness(4);
+	body.setPosition(sf::Vector2f(-50,0));
+	body.setSize(sf::Vector2f(240, 504));
+
+	icon.setFillColor(FillColor);
+	icon.setOutlineColor(BorderColor);
+	icon.setOutlineThickness(3);
+	icon.setPosition(sf::Vector2f(0, 0));
+	icon.setSize(sf::Vector2f(50, 50));
+
+	icon_text.setString(sf::String(std::wstring(L"\uf013")));
+	icon_text.setPosition(sf::Vector2f(0, 0));
+
+	remove_icon_text.setString(sf::String(std::wstring(L"\uf013")));
+	remove_icon_text.setPosition(sf::Vector2f(240, 252));
+
+}
+
+MainMenu::~MainMenu() {
+
+}
+
+void MainMenu::handleEvent(sf::Event event) {
+
+}
--- MainMenu.h
+++ MainMenu.h
@@ -5,9 +5,42 @@
 #ifndef CHESS_SFML_MAINMENU_H
 #define CHESS_SFML_MAINMENU_H
 
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ResourceHolder.h"
+#include "IListener.h"
 
-class MainMenu {
+class MainMenu : public sf::Drawable , public IListener {
+	sf::RectangleShape body;         
+	sf::RectangleShape icon;          
+	sf::RectangleShape remove_icon;  
+	sf::Text icon_text;
+	sf::Text remove_icon_text;
 
+	const sf::Color FillColor = sf::Color(0 , 0 , 48); //Gray
+	const sf::Color BorderColor = sf::Color::Black;
+	const sf::Color BorderColorHover = sf::Color(0, 0, 80);
+
+public:
+	MainMenu::MainMenu();
+
+	template <class T>
+	bool contains(sf::Vector2<T> point) {
+		return body.getGlobalBounds().contains(point.x, point.y);
+	}
+
+	void handleEvent(sf::Event event) override;
+
+	~MainMenu() override;
+
+protected:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
+		target.draw(body);
+		target.draw(icon);
+		target.draw(remove_icon);
+		target.draw(icon_text);
+		target.draw(remove_icon_text);
+	}
 };
 
 
